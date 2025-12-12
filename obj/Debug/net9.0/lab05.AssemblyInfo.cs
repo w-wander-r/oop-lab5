bf4af159ef1bf23a8b0c8d66cf9a1f5b54fc8857
w-wander-r <wanderexe68@gmail.com>
2025-12-12 13:12:14 +0200

    MyFrac & MyComplex added

--- obj/Debug/net9.0/lab05.AssemblyInfo.cs
+++ obj/Debug/net9.0/lab05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b6757e4640f5bb3462b18d848ba99c00407ca7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ead76f402182d660457a364bb3ad5e14ef33ee3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
